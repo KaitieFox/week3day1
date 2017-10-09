692e9841e48f7bf2602eee50a1df936e48914ef7
KaitieFox <kaitiefox@gmail.com>
2017-10-09 11:13:16 -0400

    Initial Commit

---
+++ week3day1/Cat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3day1
+{
+    class Cat
+    {
+        //STATES
+        //two types of states:: 
+        //::fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+
+        //::properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        //make one property for each field.
+        //so can only access name through deafault constructor at this point.
+
+
+
+
+
+        //BEHAVIOURS
+
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor because it takes no parameters in parentheses
+            //inside curly braces are called body.
+            //default constructor title hinges on parentheses.
+            //can still have stuff in body
+        }
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+            //we are creating two ways to create a cat object. one default way, one where we can assign value immediately.
+            //This allows you to very quickly create similar objects. The two methods.
+            //You always need a default constructor.
+            //this. is called a pointer. Involves specificity.
+        }
+
+        //Methods. The actiony bits.
+
+        public void Eat()
+        {
+            if(isHungry == true)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry? " + isHungry );
+        }
+
+        public void WorkOut()
+        {
+            if(isHungry == false)
+            {
+                isHungry = true;
+            }
+            Console.WriteLine("The cat worked out. Hungry now? " + isHungry);
+        }
+
+
+
+    }
+}
---
+++ week3day1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat mittens = new week3day1.Cat();  //this creates the name for the space which contains the cat information.
+
+            mittens.Name = "Mittens"; //we assign the name here. mittens is the space which presently holds the cat info.
+            //"Mittens" makes the name portion of the info stored in mittens to "Mittens".
+
+            Cat secondCat = new Cat("Oz", 3, "orange and white"); //must be in order
+            //Now we have created two cats, using first and second methods
+
+            Console.WriteLine(mittens.Name);
+            Console.WriteLine(secondCat.Name);
+
+            Console.WriteLine("I want to feed the cat.");
+            Console.WriteLine("Is " + mittens.Name + " hungry?" );
+
+            mittens.Eat();
+
+            mittens.WorkOut();
+
+            mittens.Eat();
+
+            mittens.WorkOut();
+
+
+
+
+
+
+
+        }
+    }
+}
